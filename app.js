d27d7dab38c13898b1e5acc9595692f1aa7d6faf
Raydan Bala <Raydanb@gmail.com>
2021-07-10 10:55:45 -1000

    working board, added reset button

--- app.js
+++ app.js
@@ -15,6 +15,9 @@ function resetState() {
 let boardElem = document.querySelector('#board');
 console.log('boardElem', boardElem);
 
+let resetButtonElem = document.querySelector('#resetButton');
+console.log('resetButtonElem', resetButtonElem);
+
 // DOM MANIPULATION FUNCTIONS
 function renderBoard() {
    // empty the element
@@ -29,6 +32,23 @@ function renderBoard() {
      boardElem.appendChild(cellElem);
     }
 }
+
+// EVENT LISTENERS
+boardElem.addEventListener('click', function(event) {
+    let cellIdx = event.target.dataset.index;
+    
+    if(event.target.className === 'cell' && state.board[cellIdx] === null) {
+        console.log('event.target: ', event.target);
+        console.log('cellIdx: ', cellIdx);
+        state.board[cellIdx] = 'X'
+        renderBoard();
+      }
+})
+
+resetButtonElem.addEventListener('click', function(event) {
+    resetState();
+    renderBoard();
+})
 
 // BOOTSTRAPPING
 resetState();
